d30cb8b9a20e08f8c6f13d985fa27e61f7990eed
dariabotez <93612092+dariabotez@users.noreply.github.com>
2021-11-07 22:33:53 +0200

    Add files via upload

---
+++ tema1.py
@@ -0,0 +1,24 @@
+mylist= [7,8,9,2,3,1,4,10,5,6]
+mylist.sort()
+print(mylist)
+
+mylist.sort(reverse= True)
+print(mylist)
+
+print(mylist[::2])
+
+print(mylist[1::2])
+
+for num in mylist:
+    if num %3 == 0:
+     print(num)
+
+
+
+
+
+
+
+
+
+
